72f844b4869a4f8609cc81d76eb25abf9c8a7d0d
yordi1616 <yordibuzayehu6@gmail.com>
2025-08-19 03:12:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+# --- Routes ---
 
 @app.route('/')
-def index():
-    return render_template('login.html')
+def home_redirect():
+    game_data = get_game_data(request)
+    if not game_data.get('username'):
+        return redirect(url_for('login'))
+    return redirect(url_for('game'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if not username:
+            flash("Please enter a username!")
+            return render_template('login.html')
+        
+        game_data = get_game_data(request)
+        game_data['username'] = username # Set username
+        
+        # Initialize default game state for new users if it's their first time
+        if game_data.get('score') is None:
+            game_data['score'] = 0
+            game_data['per_tap_bonus'] = 1
+            game_data['current_tap_limit'] = DEFAULT_TAP_LIMIT
+            game_data['taps_left'] = DEFAULT_TAP_LIMIT
+            game_data['task_completed'] = False
+            game_data['purchased_tap_upgrades'] = []
+            game_data['purchased_limit_upgrades'] = []
+            
+        # Update last visit time upon login
+        game_data['last_visit'] = datetime.now().isoformat()
+
+        response = make_response(redirect(url_for('game')))
+        return save_game_data(response, game_data)
+
+    return render_template('login.html')
